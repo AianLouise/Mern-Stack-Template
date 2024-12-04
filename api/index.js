0cb752c2abbd3c589c7fe5be64facd138478f9d7
AianLouise <aianlouisealfaro03@gmail.com>
2024-12-04 14:01:28 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,19 +21,7 @@ mongoose.connect(process.env.MONGODB_URI)
 // Middleware
 const allowedOrigins = [process.env.CLIENT_URL, 'https://mern-stack-template.vercel.app'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    console.log('Origin:', origin); // Log the origin for debugging
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
